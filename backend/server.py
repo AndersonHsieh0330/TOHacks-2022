fac7ff3c4ebc1982fc699770b6ec950c1e957094
Jeff <guywholikessoda@gmail.com>
2022-05-28 20:08:47 -0400

    api: establish GET/POST endpoints

--- backend/server.py
+++ backend/server.py
@@ -1,38 +1,70 @@
 import flask
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify, Response
 from os import environ
+import time
+import random
+import logging
+import os
+from argparse import ArgumentParser, RawTextHelpFormatter
+import db
 
-DEFAULT_ROUTE_LEADERBOARD = "index"
-DEFAULT_ROUTE_PLAYER = "player"
+import psycopg2
+from psycopg2.errors import SerializationFailure
 
 app = Flask(__name__)
 
-# conn_string = environ.get("DB_URI")
+opt = db.parse_cmdline()
+logging.basicConfig(level=logging.DEBUG if opt.verbose else logging.INFO)
+
+try:
+    # Attempt to connect to cluster with connection string provided to
+    # script. By default, this script uses the value saved to the
+    # DATABASE_URL environment variable.
+    # For information on supported connection string formats, see
+    # https://www.cockroachlabs.com/docs/stable/connect-to-the-database.html.
+    db_url = opt.dsn
+    conn = psycopg2.connect(db_url)
+except Exception as e:
+    logging.fatal("Database connection failed")
+    logging.fatal(e)
+
 
 @app.route("/")
 def index():
     pass
-    # scores = leaderboard.get_scores()
     # return render_template("index.html",
     #                         scores=scores)
 
-@app.route("/placeholder", methods=["GET", "POST"])
+
+@app.route("api/activity", methods=["GET", "POST"])
 def placeholder():
-    pass
-# def player():
-#     if flask.request.method == "POST":
-#         id = flask.request.values.get("id")
-#         avatar = flask.request.values.get("avatar")
-#         playername = flask.request.values.get("playername")
-#         points = flask.request.values.get("points")
-#         leaderboard.add_score(
-#             Score(id=id, avatar=avatar, playername=playername, points=points)
-#         )
-#         return redirect(url_for(DEFAULT_ROUTE_LEADERBOARD))
-#     else:
-#         avatars = leaderboard.get_avatar_dic()
-#         score = Score(avatar="0", playername="", points=0)
-#         return render_template("player.html", score = score, avatars = avatars)
+    if flask.request.method == "POST":
+        activity = flask.request.json['activity']
+        category = flask.request.json['category']
+
+        entry = {
+            'activity': activity,
+            'category': category,
+        }
+
+        db.insert_activity(conn, entry)
+
+    else:
+        result = db.return_activity(conn)
+
+        jresult = {
+            'activity': result[0],
+            'location': result[1],
+            'transportation': result[2]
+        }
+
+        return jresult
+
+    # return redirect(url_for(DEFAULT_ROUTE_LEADERBOARD))
+    # return render_template("player.html", score = score, avatars = avatars)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    db.init_db(conn)
+    db.print_activities(conn)
+    app.run(debug=True, port="8080")
